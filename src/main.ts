8c68787832e85718bccc515ab346ad144354082f
Pascal Grimmiger <81360569+pascalgrim@users.noreply.github.com>
2023-12-02 05:02:35 +0100

    port

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3002);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
